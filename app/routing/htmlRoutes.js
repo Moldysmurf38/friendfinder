06a71ee3ab529d1b4489bbc1986d269c512e5453
David <davwil38@gmail.com>
2019-09-10 14:16:55 -0400

    index restructure

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,9 +2,9 @@ var path = require("path");
 
 module.exports = function (app) {
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/views/survey.html"));
     });
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/index.html"));
+        res.sendFile(path.join(__dirname, "/../public/views/index.html"));
     });
 }
